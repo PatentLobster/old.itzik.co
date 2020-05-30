9bc6fe61bdd16c1c12020d5327440c028ecf8697
PatentLobster <37775846+PatentLobster@users.noreply.github.com>
2020-05-31 00:44:15 +0300

    cleanup

--- src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - itzik.co",
-  "siteUrl": "",
-  "version": "0.7.15",
-  "catchLinks": true
-}
--- src/.temp/now.js
+++
@@ -1,1 +0,0 @@
-export default 1590820132961
