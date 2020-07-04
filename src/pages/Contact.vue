f3086917daf6368d7b820dc65d7d0e8216a700c6
PatentLobster <itzik@imusli.com>
2020-07-04 23:20:14 +0300

    New contact form

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -4,33 +4,56 @@
     <h2>Hey! 🖐</h2>
     <h3>Can't wait to hear from you.</h3>
     <h4>you can mail me at <g-link href="mailto:contact@itzik.co">contact@itzik.co</g-link> or just use the form below.</h4>
-    <div class="panel">
-      <div class="state"><br><i class="fa fa-envelope-square"></i><br><h1>Contact</h1></div>
-      <div class="form">
-        <form
-                action="https://send.pageclip.co/4zSBzG3af0NkVOL1Z9lcA01mrKEJdSoB"
-                class="pageclip-form"
-                method="post"
-        >
-          <p v-if="errors.length">
-            <b>Please correct the following error(s):</b>
-          <ul>
-            <li v-for="error in errors">{{ error }}</li>
-          </ul>
-          </p>
-          <input type="text" name="name" placeholder="Chuck Norris" v-model="name"/>
-          <input type="email" name="email" placeholder="your@email.com" v-model="email"/>
-          <textarea name="content" v-model="content"></textarea>
-          <button type="submit" value="Submit" class="pageclip-form__submit send">
-            <span>Send</span>
-          </button>
-        </form>
+    <div class="ifle" >
+    <form class="vue-form"
+          @submit="submit"
+          action="https://send.pageclip.co/4zSBzG3af0NkVOL1Z9lcA01mrKEJdSoB"
+          method="post"
+    >
+
+      <div class="error-message">
+        <p v-show="!email.valid">Oh, please enter a valid email address.</p>
       </div>
+
+      <fieldset>
+        <legend>The Form 😁</legend>
+        <div>
+          <label class="label" for="name">Name</label>
+          <input type="text" name="name" id="name" placeholder="Chuck Norris" required="" v-model="name">
+        </div>
+        <div>
+          <label class="label" for="email">Email</label>
+          <input type="email" name="email" id="email" required=""
+                 :class="{ email , error: !email.valid }"
+                 v-model="email.value">
+        </div>
+        <div>
+
+        </div>
+
+
+        <div>
+          <label class="label" for="textarea">Message</label>
+          <textarea class="message" name="textarea" id="textarea" required=""
+                    v-model="message.text"
+                    :maxlength="message.maxlength"></textarea>
+          <span class="counter">{{ message.text.length }} / {{ message.maxlength }}</span>
+        </div>
+        <div>
+          <input type="submit" value="Send">
+        </div>
+      </fieldset>
+    </form>
+
+    <div class="debug">
+      <pre><code>{{ $data }}</code></pre>
     </div>
+      </div>
   </Layout>
 </template>
 
 <script>
+  const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 export default {
   metaInfo: {
     title: 'Contact Me.',
@@ -38,29 +61,39 @@ export default {
   },
   data: () => {
     return {
-    errors: [],
-    name: [],
-    email: [],
-    content: [],
-    }
+      name: "",
+      email: {
+        value: "",
+        valid: true
+      },
+      message: {
+        text: ``,
+        maxlength: 1000,
+        minLength: 50
+      },
+      submitted: false
+    };
   },
   methods: {
-    checkForm(e) {
-      this.errors = [];
-      if (!this.name || this.name.length <= 2)
-        this.errors.push("Name required.");
-      if (!this.email || !this.validEmail(this.email))
-        this.errors.push("Valid Email required.");
-      if (!this.content)
-        this.errors.push("Message body required.");
-      if (this.content.length < 50)
-        this.errors.push("Message body min length is 50.");
-      if(this.errors)
-        e.preventDefault();
+    // submit form handler
+    submit: function() {
+      this.submitted = true;
     },
-    validEmail: function (email) {
-      var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return re.test(email);
+    // validate by type and value
+    validate: function(type, value) {
+      if (type === "email") {
+        this.email.valid = this.isEmail(value);
+      }
+    },
+    // check for valid email adress
+    isEmail: function(value) {
+      return emailRegExp.test(value);
+    },
+  },
+  watch: {
+    // watching nested property
+    "email.value": function(value) {
+      this.validate("email", value);
     }
   }
 }
@@ -68,126 +101,310 @@ export default {
 
 <style scoped type="scss">
   @import "https://s.pageclip.co/v1/pageclip.css";
-  .panel *:focus {
-    box-shadow: 0 0 5px rgba(81, 203, 238, 1);
-    border: 1px solid rgba(81, 203, 238, 1);
+  @import url("https://fonts.googleapis.com/css?family=Source+Code+Pro:300,400");
+  .ifle {
+    display: inline-flex;
   }
-  .panel {
-    width: 400px;
-    -webkit-border-radius: 10px;
-    -moz-border-radius: 10px;
-    border-radius: 10px;
-    box-shadow: var(--bshadow);
-    background: var(--bg-teritary);
-    color: var(--body-color);
+  @media only screen and (max-width: 768px) {
+    .ifle {
+      /*display: block;*/
+      max-width: 70vw;
+      /*text-align: center;*/
+      /*margin: 0 auto;*/
+
+    }
+
+    .debug, code {
+      display: none;
+    }
+  }
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }
+
+
+
+  .vue-form {
+    font-size: 16px;
+    width: 500px;
+    padding: 15px 30px;
+    border-radius: 4px;
     margin: 50px auto;
-    padding-bottom: 1rem;
-    text-align: center;
-  }
-  .panel .state {
-    margin-top: 5px;
-    width: 100%;
-    height: 155px;
+    width: 500px;
+    /*background-color: #fff;*/
+    background-color: var(--bg-secondary);
     color: var(--body-color);
-    font-size: 20px;
+    box-shadow: 0 4px 6px 0 rgba(0, 0, 0, 0.3);
   }
-  .panel .state i {
-    font-size: 40px;
-  }
-  .panel .state i {
-    color: var(--body-color);
-    line-height: 33px;
-    height: 30px;
-    width: 30px;
-    display: inline-block;
-  }
-  .panel .state h2 {
-    font-weight: 400;
-  }
-  .panel .form {
-    width: 340px;
-    margin: 5px auto;
-  }
-  .panel .send {
-    height: 45px;
-    width: 100%;
-    background-color: #8bc34a;
-    -webkit-border-radius: 45px;
-    -moz-border-radius: 45px;
+  .vue-form fieldset {
+    margin: 24px 0 0 0;
     border: 0;
-    border-radius: 45px;
+  }
+  .vue-form legend {
+    padding-bottom: 10px;
+    /*border-bottom: 1px solid #ecf0f1;*/
+  }
+  .vue-form div {
     position: relative;
-    line-height: 45px;
-    text-align: center;
-    font-weight: bold;
-    color: white;
-    margin-top: 10px;
-    -webkit-transition: background .2s;
-    -moz-transition: background .2s;
-    -o-transition: background .2s;
-    transition: background .2s;
+    margin: 20px 0;
+  }
+  .vue-form h4,
+  .vue-form .label {
+    color: var(--body-color);
+    margin-bottom: 10px;
+  }
+  .vue-form .label {
+    display: block;
+  }
+  .vue-form input,
+  .vue-form textarea,
+  .vue-form select,
+  .vue-form label {
+    color: var(--body-color);
+  }
+  .vue-form input[type="text"],
+  .vue-form input[type="email"],
+  .vue-form textarea,
+  .vue-form select,
+  .vue-form legend {
+    display: block;
+    width: 100%;
+    appearance: none;
+  }
+  .vue-form input[type="text"],
+  .vue-form input[type="email"],
+  .vue-form textarea,
+  .vue-form select {
+    padding: 12px;
+    border: 1px solid var(--bg);
+    /*background-color: #ffffff;*/
+    background-color: var(--bg-teritary);
+    color: var(--body-color);
+    border-radius: 0.25em;
+    box-shadow: inset 0 1px 1px var(--bg);
+  }
+  .vue-form input[type="text"]:focus,
+  .vue-form input[type="email"]:focus,
+  .vue-form textarea:focus,
+  .vue-form select:focus {
+    outline: none;
+    border-color: #2c3e50;
+    box-shadow: 0 0 5px rgba(44, 151, 222, 0.2);
+  }
+  .vue-form .select {
+    position: relative;
+  }
+  .vue-form .select::after {
+    content: "";
+    position: absolute;
+    z-index: 1;
+    right: 16px;
+    top: 50%;
+    margin-top: -8px;
+    display: block;
+    width: 16px;
+    height: 16px;
+    background: url("data:image/svg+xml;charset=utf-8,%3C%3Fxml%20version%3D%221.0%22%20encoding%3D%22utf-8%22%3F%3E%0D%0A%3C%21DOCTYPE%20svg%20PUBLIC%20%22-%2F%2FW3C%2F%2FDTD%20SVG%201.1%2F%2FEN%22%20%22http%3A%2F%2Fwww.w3.org%2FGraphics%2FSVG%2F1.1%2FDTD%2Fsvg11.dtd%22%3E%0D%0A%3Csvg%20version%3D%221.1%22%20id%3D%22Layer_1%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20xmlns%3Axlink%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink%22%20x%3D%220px%22%20y%3D%220px%22%0D%0A%09%20width%3D%2216px%22%20height%3D%2216px%22%20viewBox%3D%220%200%2016%2016%22%20enable-background%3D%22new%200%200%2016%2016%22%20xml%3Aspace%3D%22preserve%22%3E%0D%0A%3Cg%3E%0D%0A%09%3Cpolygon%20fill%3D%22%232c3e50%22%20points%3D%220.9%2C5.5%203.1%2C3.4%208%2C8.3%2012.9%2C3.4%2015.1%2C5.5%208%2C12.6%20%09%22%2F%3E%0D%0A%3C%2Fg%3E%0D%0A%3C%2Fsvg%3E")
+    no-repeat center center;
+    pointer-events: none;
+  }
+  .vue-form select {
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.08);
     cursor: pointer;
   }
-  .panel .send:active {
-    -webkit-transform: translateY(2px);
-    -moz-transform: translateY(2px);
-    -ms-transform: translateY(2px);
-    -o-transform: translateY(2px);
-    transform: translateY(2px);
+  .vue-form select::-ms-expand {
+    display: none;
   }
-  .panel .send:hover {
-    background-color: #599b2d;
+  .vue-form .vue-form-list {
+    margin-top: 16px;
   }
-  .panel input {
-    background: var(--bg);
-    -webkit-border-radius: 45px;
-    -moz-border-radius: 45px;
-    border-radius: 45px;
-    font-size: 15px;
-    height: 45px;
-    color: var(--body-color);
+  .vue-form .vue-form-list::after {
+    clear: both;
+    content: "";
+    display: table;
+  }
+  .vue-form .vue-form-list li {
+    display: inline-block;
+    position: relative;
+    user-select: none;
+    margin: 0 26px 16px 0;
+    float: left;
+  }
+  .vue-form input[type="radio"],
+  .vue-form input[type="checkbox"] {
+    position: absolute;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    margin: 0;
+    padding: 0;
+    opacity: 0;
+    z-index: 2;
+  }
+  .vue-form input[type="radio"] + label,
+  .vue-form input[type="checkbox"] + label {
+    padding-left: 24px;
+  }
+  .vue-form input[type="radio"] + label::before,
+  .vue-form input[type="radio"] + label::after,
+  .vue-form input[type="checkbox"] + label::before,
+  .vue-form input[type="checkbox"] + label::after {
+    content: "";
+    display: block;
+    position: absolute;
+    left: 0;
+    top: 50%;
+    margin-top: -8px;
+    width: 16px;
+    height: 16px;
+  }
+  .vue-form input[type="radio"] + label::before,
+  .vue-form input[type="checkbox"] + label::before {
+    border: 1px solid #cfd9db;
+    background: #ffffff;
+    box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.08);
+  }
+  .vue-form input[type="radio"] + label::before,
+  .vue-form input[type="radio"] + label::after {
+    border-radius: 50%;
+  }
+  .vue-form input[type="checkbox"] + label::before,
+  .vue-form input[type="checkbox"] + label::after {
+    border-radius: 0.25em;
+  }
+  .vue-form input[type="radio"] + label::after,
+  .vue-form input[type="checkbox"] + label::after {
+    background-color: #2c3e50;
+    background-position: center center;
+    background-repeat: no-repeat;
+    box-shadow: 0 0 5px rgba(44, 151, 222, 0.4);
+    display: none;
+  }
+  .vue-form input[type="radio"] + label::after {
+    background-image: url("data:image/svg+xml;charset=utf-8,%3C%3Fxml%20version%3D%221.0%22%20encoding%3D%22utf-8%22%3F%3E%0D%0A%3C%21DOCTYPE%20svg%20PUBLIC%20%22-%2F%2FW3C%2F%2FDTD%20SVG%201.1%2F%2FEN%22%20%22http%3A%2F%2Fwww.w3.org%2FGraphics%2FSVG%2F1.1%2FDTD%2Fsvg11.dtd%22%3E%0D%0A%3Csvg%20version%3D%221.1%22%20id%3D%22Layer_1%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20xmlns%3Axlink%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink%22%20x%3D%220px%22%20y%3D%220px%22%0D%0A%09%20width%3D%2216px%22%20height%3D%2216px%22%20viewBox%3D%220%200%2016%2016%22%20enable-background%3D%22new%200%200%2016%2016%22%20xml%3Aspace%3D%22preserve%22%3E%0D%0A%3Ccircle%20fill%3D%22%23FFFFFF%22%20cx%3D%228%22%20cy%3D%228%22%20r%3D%223%22%2F%3E%0D%0A%3C%2Fsvg%3E");
+  }
+  .vue-form input[type="checkbox"] + label::after {
+    background-image: url("data:image/svg+xml;charset=utf-8,%3C%3Fxml%20version%3D%221.0%22%20encoding%3D%22utf-8%22%3F%3E%0D%0A%3C%21--%20Generator%3A%20Adobe%20Illustrator%2018.1.1%2C%20SVG%20Export%20Plug-In%20.%20SVG%20Version%3A%206.00%20Build%200%29%20%20--%3E%0D%0A%3C%21DOCTYPE%20svg%20PUBLIC%20%22-%2F%2FW3C%2F%2FDTD%20SVG%201.1%2F%2FEN%22%20%22http%3A%2F%2Fwww.w3.org%2FGraphics%2FSVG%2F1.1%2FDTD%2Fsvg11.dtd%22%3E%0D%0A%3Csvg%20version%3D%221.1%22%20id%3D%22Layer_1%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20xmlns%3Axlink%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink%22%20x%3D%220px%22%20y%3D%220px%22%0D%0A%09%20width%3D%2216px%22%20height%3D%2216px%22%20viewBox%3D%220%200%2016%2016%22%20enable-background%3D%22new%200%200%2016%2016%22%20xml%3Aspace%3D%22preserve%22%3E%0D%0A%3Cpolyline%20fill%3D%22none%22%20stroke%3D%22%23FFFFFF%22%20stroke-width%3D%222%22%20stroke-linecap%3D%22square%22%20stroke-miterlimit%3D%2210%22%20points%3D%225%2C8%207%2C10%2011%2C6%20%22%2F%3E%0D%0A%3C%2Fsvg%3E");
+  }
+  .vue-form input[type="radio"]:focus + label::before,
+  .vue-form input[type="checkbox"]:focus + label::before {
+    box-shadow: 0 0 5px rgba(44, 151, 222, 0.6);
+  }
+  .vue-form input[type="radio"]:checked + label::after,
+  .vue-form input[type="checkbox"]:checked + label::after {
+    display: block;
+  }
+  .vue-form input[type="radio"]:checked + label::before,
+  .vue-form input[type="radio"]:checked + label::after,
+  .vue-form input[type="checkbox"]:checked + label::before,
+  .vue-form input[type="checkbox"]:checked + label::after {
+    animation: cd-bounce 0.3s;
+  }
+  .vue-form textarea {
+    min-height: 120px;
+    resize: vertical;
+    overflow: auto;
+  }
+  .vue-form input[type="submit"] {
     border: none;
-    padding-left: 15px;
-    width: -webkit-calc(100% - 15px);
-    width: -moz-calc(100% - 15px);
-    width: calc(100% - 15px);
-    margin-bottom: 10px;
+    background: #2c3e50;
+    border-radius: 0.25em;
+    padding: 12px 20px;
+    color: #ffffff;
+    font-weight: bold;
+    float: right;
+    cursor: pointer;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    appearance: none;
   }
-  .panel input[placeholder] {
+  .no-touch .vue-form input[type="submit"]:hover {
+    background: #42a2e1;
+  }
+  .vue-form input[type="submit"]:focus {
+    outline: none;
+    background: #2b3e51;
+  }
+  .vue-form input[type="submit"]:active {
+    transform: scale(0.9);
+  }
+  .vue-form .error-message {
+    height: 35px;
+    margin: 0px;
+  }
+  .vue-form .error-message p {
+    background: #e94b35;
+    display: inline;
+    color: #ffffff;
+    font-size: 1.4rem;
+    text-align: center;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    border-radius: 0.25em;
+    padding: 16px;
+  }
+  .vue-form .error {
+    border-color: #e94b35 !important;
+  }
+  .vue-form .counter {
+    /*background-color: #ecf0f1;*/
+    background-color: var(--bg-teritary);
     color: var(--body-color);
-    font-size: 15px;
-    font-weight: 500;
+    position: absolute;
+    right: 0px;
+    top: 0px;
+    font-size: 10px;
+    padding: 4px;
   }
-  .panel textarea {
-    color: var(--body-color);
-    background: var(--bg);
-    -webkit-border-radius: 45px;
-    -moz-border-radius: 45px;
-    border-radius: 10px;
-    font-size: 15px;
-    height: 200px;
-    border: none;
-    padding-left: 15px;
-    width: -webkit-calc(100% - 15px);
-    width: -moz-calc(100% - 15px);
-    width: calc(100% - 15px);
-    margin-bottom: 10px;
+
+  .debug {
+    border-radius: 4px;
+    margin: 50px auto;
+    width: 500px;
+    background-color: #000;
+    padding: 50px;
+    background: rgba(0, 0, 0, 0.8);
+    box-shadow: 0 4px 6px 0 rgba(0, 0, 0, 0.3);
   }
-  .panel .fack {
-    margin-top: 30px;
-    font-size: 14px;
+
+  .debug pre {
+    color: #ffffff;
+    font-size: 18px;
+    text-align: left;
+    line-height: 30px;
+    font-family: "Source Code Pro", monospace;
+    font-weight: 300;
+    white-space: pre-wrap;
   }
-  .panel .fack i.fa {
-    text-decoration: none;
-    color: var(--body-color);
-    vertical-align: middle;
-    font-size: 20px;
-    margin-right: 5px;
+
+  @-webkit-keyframes cd-bounce {
+    0%,
+    100% {
+      -webkit-transform: scale(1);
+    }
+    50% {
+      -webkit-transform: scale(0.8);
+    }
   }
-  .panel .fack a:link {
-    color: #616973;
+  @-moz-keyframes cd-bounce {
+    0%,
+    100% {
+      -moz-transform: scale(1);
+    }
+    50% {
+      -moz-transform: scale(0.8);
+    }
   }
-  .panel .fack a:visited {
-    color: #555c65;
+  @keyframes cd-bounce {
+    0%,
+    100% {
+      transform: scale(1);
+    }
+    50% {
+      transform: scale(0.8);
+    }
   }
+
 </style>
